aca8a72e95408829a771a42355349e7a7b092ddb
Freddy Marcano <freddyaranguiz26@gmail.com>
2025-06-02 09:01:44 -0400

    cambios2/06 9:01

--- Start.cpp
+++ Start.cpp
@@ -3,9 +3,15 @@ using namespace std;
 
 int main(){
 
+    int num1 = 0; 
+
     cout << "Hola" << endl;
     cout << "Mundo" << endl;
     cout << "C++"; 
+    cin >> num1; 
+
+    cout << num1; 
+
 
     return 0; 
 }
